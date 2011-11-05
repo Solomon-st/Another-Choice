29dec6de99b20552f7289472e428d366d2dcb1f6
Solomon-st <Solomon.Royarr@gmail.com>
2011-11-05 11:36:40 +0800

    first

---
+++ src/com/sol/ac/AnotherChoiceActivity.java
@@ -0,0 +1,13 @@
+package com.sol.ac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherChoiceActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
